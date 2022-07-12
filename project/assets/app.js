951be888dd4b303d0bda99c9bc652e94ae9b9fdf
bendatiy <bendatiy.v@toplyvo.app>
2022-07-12 20:45:49 +0300

    add bootstrap & fontawesome

--- project/assets/app.js
+++ project/assets/app.js
@@ -8,5 +8,5 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
-// start the Stimulus application
-import './bootstrap';
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
